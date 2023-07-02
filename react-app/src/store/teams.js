5cf0657b8fa591ffb0a656a00524da725df86dbb
ChrisEkeDev <iam@chriseke.com>
2023-07-02 15:58:55 -0500

    Finished creating channel

--- react-app/src/store/teams.js
+++ react-app/src/store/teams.js
@@ -2,6 +2,7 @@
 const GET_TEAMS = '/teams/GET_TEAMS'
 const GET_TEAM = '/teams/GET_TEAM'
 const CREATE_TEAM = '/teams/CREATE_TEAM'
+const GET_EVERY_TEAM = '/teams/GET_EVERY_TEAM'
 const CLEAR_TEAM = '/teams/CLEAR_TEAM'
 
 // ACTIONS
@@ -13,6 +14,11 @@ const actionGetTeams = (teams) => ({
 const actionGetTeam = (team) => ({
     type: GET_TEAM,
     payload: team
+})
+
+const actionGetEveryTeam = (teams) => ({
+    type: GET_EVERY_TEAM,
+    payload: teams
 })
 
 const actionCreateTeam = (team) => ({
@@ -56,6 +62,22 @@ export const thunkGetTeam = (id) => async dispatch => {
     }
 }
 
+
+export const thunkGetEveryTeam = () => async dispatch => {
+    const res = await fetch(`/api/teams/`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+    })
+    if (res.ok) {
+        const data = await res.json()
+        if (data.errors) {
+            return data.errors
+        }
+        dispatch(actionGetEveryTeam(data))
+        return data
+    }
+}
+
 export const thunkCreateTeam = (team) => async dispatch => {
     const res = await fetch(`/api/teams/`, {
         method: "POST",
@@ -72,19 +94,39 @@ export const thunkCreateTeam = (team) => async dispatch => {
     }
 }
 
+export const thunkJoinTeam = (id) => async dispatch => {
+    const res = await fetch(`/api/teams/${id}/members`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+    })
+    if (res.ok) {
+        const data = await res.json()
+        if (data.errors) {
+            return data.errors
+        }
+        return data
+    }
+
+}
+
 export const clearTeam = () => async dispatch => {
     dispatch(actionClearTeam())
 }
 
 
 // REDUCER
-const initialState = { allTeams: {}, singleTeam: {} }
+const initialState = { allTeams: {}, singleTeam: {}, everyTeam: {} }
 
 const teams = (state = initialState, action) => {
     switch(action.type) {
         case GET_TEAMS: {
             const newState = { ...state, allTeams: {} }
             action.payload.forEach(team => newState.allTeams[team.id] = team)
+            return newState
+        }
+        case GET_EVERY_TEAM: {
+            const newState = { ...state, everyTeam: {} }
+            action.payload.forEach(team => newState.everyTeam[team.id] = team)
             return newState
         }
         case GET_TEAM: {
--- react-app/src/views/dashboard/index.js
+++ react-app/src/views/dashboard/index.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory, Redirect } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
-import { thunkGetTeams } from '../../store/teams';
+import { thunkGetEveryTeam, thunkGetTeams, thunkJoinTeam } from '../../store/teams';
 import { logout } from "../../store/session";
-import { FaArrowRight } from 'react-icons/fa'
+import { FaArrowRight, FaSearch } from 'react-icons/fa'
 import './dashboard.css';
 
 
 function Dashboard() {
     const [ loading, setLoading ] = useState(true)
+    const [ teamlist, setTeamList ] = useState(false);
+    const [ teamQuery, setTeamQuery ] = useState('')
     const sessionUser = useSelector((state) => state.session.user);
     const userTeams = useSelector((state) => state.teams.allTeams)
+    const everyTeam = useSelector(state => state.teams.everyTeam)
     const normalizedTeams = Object.values(userTeams)
+    const normalizedEveryTeam = Object.values(everyTeam)
     const dispatch = useDispatch();
     const history = useHistory();
+
+    console.log(normalizedEveryTeam)
 
     const handleLogout = (e) => {
         e.preventDefault();
@@ -21,8 +27,14 @@ function Dashboard() {
         dispatch(logout())
     };
 
-    const handleTeam = (id) => {
+    const handleGoToTeam = (id) => {
         history.push(`/team/${id}`)
+    }
+
+    const handleJoinTeam = (id) => {
+        console.log(id)
+        dispatch(thunkJoinTeam(id))
+        .then(() => handleGoToTeam(id))
     }
 
     const handleCreateTeam = () => {
@@ -31,6 +43,7 @@ function Dashboard() {
 
     useEffect(() => {
         dispatch(thunkGetTeams())
+        .then(() => dispatch(thunkGetEveryTeam()))
         .then(() => setLoading(false))
     }, [dispatch])
 
@@ -50,12 +63,48 @@ function Dashboard() {
                         <h1>Welcome back. You smell wonderful.</h1>
                         <p>Choose a workspace below to get back to working with your team</p>
                     </div>
-                    <div className='dashboard--teams'>
+                    {
+                        teamlist ?
+                        <div className='dashboard--teams'>
+                            <header className='dashboard--teams_header'>
+                                <div className='dashboard--teams_input'>
+                                    <FaSearch className='dashboard--teams_input_icon'/>
+                                    <input placeholder="Search All Teams" className='dashboard--teams_input_input' value={teamQuery} onChange={(x) => setTeamQuery(x.target.value)}/>
+                                </div>
+                                <button onClick={() => setTeamList(false)}>My Teams</button>
+                            </header>
+                            <div className='dashboard--teams_scroller'>
+                            {normalizedEveryTeam.map(team => (
+                                <div key={team.id} onClick={() => handleJoinTeam(team.id)} className='dashboard--team'>
+                                    <div className='dashboard--team--info'>
+                                        <div className='dashboard--team--info_image'></div>
+                                        <div>
+                                            <p>{team.name}</p>
+                                            <div className='dashboard_team_members--wrapper'>
+                                                <div className='dashboard_team_members--span'>
+                                                    {[1,2,3,4,5].map(member => (
+                                                        <div key={member} className='dashboard_team_members--member'></div>
+                                                    ))}
+                                                </div>
+                                                <span className='dashboard_team_members--count'>{team.numMembers} members</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='dashboard--team--arrow'>
+                                        <span className='open'>Join</span>
+                                        <FaArrowRight/>
+                                    </div>
+                                </div>
+                            ))}
+                            </div>
+                        </div> :
+                        <div className='dashboard--teams'>
                         <header className='dashboard--teams_header'>
                             <p>Teams for <strong>{sessionUser.email}</strong></p>
+                            <button onClick={() => setTeamList(true)}>Join a Team</button>
                         </header>
                         {normalizedTeams.map(team => (
-                            <div key={team.id}  onClick={() => handleTeam(team.id)} className='dashboard--team'>
+                            <div key={team.id}  onClick={() => handleGoToTeam(team.id)} className='dashboard--team'>
                                 <div className='dashboard--team--info'>
                                     <div className='dashboard--team--info_image'></div>
                                     <div>
@@ -77,6 +126,8 @@ function Dashboard() {
                             </div>
                         ))}
                     </div>
+                    }
+
                     <div className='dashboard--create'>
                         <div>
                             <div></div>
