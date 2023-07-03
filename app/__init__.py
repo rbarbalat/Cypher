014be29102712622783794094c86bbad5cd2444a
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-07-03 09:59:21 -0700

    doc: add entializer for the socketio

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from flask import Flask
-# from flask_socketio import SocketIO
+from .socket import socketio
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
@@ -37,6 +37,9 @@ app.register_blueprint(channel_routes, url_prefix='/api/channels')
 app.register_blueprint(direct_mes_routes, url_prefix='/api/messages')
 db.init_app(app)
 Migrate(app, db)
+
+if __name__ == "__main__":
+    socketio.run(app)
 
 # Application Security
 CORS(app)
