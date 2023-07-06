6338ea59c5b90f9581a74e027081a4847b768ac1
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-05 23:07:59 -0400

    4/4 live reply routes made but not tested (no seed data yet), user association added to live reply model

--- app/api/live_reply_routes.py
+++ app/api/live_reply_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import User, LiveReplies, db
+from app.forms.live_reply_form import LiveReplyForm
 
 live_reply_routes = Blueprint('live_replies', __name__)
 
@@ -16,3 +17,26 @@ def delete_reply(id):
   db.session.delete(reply)
   db.session.commit()
   return {"message" : "message deleted :)"}
+
+#EDIT reply by id
+@live_reply_routes.route('/<int:id>', methods=['POST'])
+def edit_reply(id):
+  if not current_user.is_authenticated:
+    return {"error": "go get logged in"}
+
+  reply = LiveReplies.query.get(id)
+  if not reply:
+    return {"error": "the reply couldn't be found"}
+
+  if reply.sender_id != current_user.id:
+    return {"error" : "not authorized"}
+
+  form = LiveReplyForm()
+  form["csrf_token"].data = request.cookies["csrf_token"]
+
+  if form.validate_on_submit():
+    reply.message = form.data['message']
+    db.session.commit()
+    return reply.to_dict_no_assoc()
+
+  return {"errors": form.errors}
