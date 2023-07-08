f0a4f136abd352fa33f9f504af0e0c4e7d141577
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-08 19:18:58 -0400

    added the add image functionality but only updated the get user thread thunk

--- react-app/src/components/thread/threadUser.js
+++ react-app/src/components/thread/threadUser.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { thunkGetUserThread } from '../../store/thread';
+import { useDispatch } from "react-redux"
 
 function ThreadUser({thread}) {
   const [image, setImage] = useState(undefined)
   const [imagePreview, setImagePreview ] = useState(undefined)
   const [errors, setErrors] = useState({});
+  const dispatch = useDispatch()
 
   const handleImage = (e) => {
     setImage(e.target.files[0])
@@ -15,15 +18,20 @@ function ThreadUser({thread}) {
     const formData = new FormData()
     formData.append("image", image)
     const newImage = formData;
+    //updates the image of the current_user
     const data = await fetch(`/api/users/image`, {
       method: 'POST',
       body: newImage
     })
+    console.log("response")
+    console.log(data)
+    console.log(thread)
     if (data.errors) {
       const valErrors = {}
       valErrors.image = data.errors["image"]
       setErrors(valErrors)
     }
+    await dispatch(thunkGetUserThread(thread.id))
   }
 
   return (
