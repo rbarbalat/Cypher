d56db29f546739efd2bcd375258c90b47b52ef0d
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-08 10:57:10 -0400

    added a unique validator for team names

--- app/forms/team_form.py
+++ app/forms/team_form.py
@@ -3,10 +3,16 @@ from wtforms import StringField, URLField
 from wtforms.validators import URL, DataRequired, ValidationError, Length
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from app.api.aws import ALLOWED_EXTENSIONS
+from app.models import Team
 
+def team_name_exists(form, field):
+    # Checking if team name is already in use
+    name = field.data
+    team = Team.query.filter(Team.name == name).first()
+    if team:
+        raise ValidationError('Team name is already in use.')
 
 class TeamForm(FlaskForm):
-    name = StringField("name", validators=[DataRequired(), Length(min=2, max=20)], )
+    name = StringField("name", validators=[DataRequired(), team_name_exists, Length(min=2, max=20)], )
     description = StringField("description", validators=[DataRequired(), Length(min=10, max=1000)] )
     image = FileField("image", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
-    # image = URLField("image")
