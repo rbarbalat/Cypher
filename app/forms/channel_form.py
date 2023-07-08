4240787a9d87c890153e131cb4b2a70209a81900
Roman Barbalat <roman.barbalat22@gmail.com>
2023-07-08 11:10:29 -0400

    added unique Channel validator

--- app/forms/channel_form.py
+++ app/forms/channel_form.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SelectField, IntegerField
 from wtforms.validators import URL, DataRequired, Email, ValidationError, Length
+from app.models import Team, Channel
+
+def channel_name_exists(form, field):
+    # Checking if channel name is already in use
+    name = field.data
+    channel = Channel.query.filter(Channel.name == name).first()
+    if channel:
+        raise ValidationError('Channel name is already in use (possibly in a different team).')
 
 class ChannelForm(FlaskForm):
-    name = StringField("name", validators=[DataRequired(), Length(min = 2, max = 20) ])
+    name = StringField("name", validators=[DataRequired(), channel_name_exists, Length(min = 2, max = 20) ])
     description = StringField("description", validators=[DataRequired(), Length(min = 10, max=1000)] )
     private = BooleanField("private")
-    # team = IntegerField("team_id", validators=[DataRequired()])
-    # team = SelectField("team", choices = [], validators = [DataRequired()])
-    #need to insert the choices inside the route handler
-    #choices is a list of tuples, first element is the db id, second the team name
